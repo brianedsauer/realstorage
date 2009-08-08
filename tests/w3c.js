144f62e1081cc56deafec8ddfd3e3095d393b72c
Brett Cannon <brett@python.org>
2009-08-08 14:21:47 -0700

    Move over to using teststore.

--- tests/w3c.js
+++ tests/w3c.js
@@ -2,25 +2,23 @@
 
 var pesky_keys = [[42, "42"], [null, "null"], [undefined, "undefined"]];
 
-var store = window.realStorage;
-
-// (At least) Safari 4 throws a fit if store.clear is passed directly
-module("W3C spec", {teardown: function() {store.clear();}});
+// (At least) Safari 4 throws a fit if teststore.clear is passed directly
+module("W3C spec", {teardown: function() {teststore.clear();}});
 
 test("setItem/getItem work for strings", function() {
     var key = "key";
     var value = "value";
 
-    store.setItem(key, value);
+    teststore.setItem(key, value);
 
-    same(store.getItem(key), value, "value set is what is gotten");
+    same(teststore.getItem(key), value, "value set is what is gotten");
 });
 
 
 test("getItem returns null for non-existent keys", function() {
-    store.clear();
+    teststore.clear();
 
-    same(store.getItem("any ol' key"), null,
+    same(teststore.getItem("any ol' key"), null,
         "null returned for non-existent key");
 });
 
@@ -30,8 +28,8 @@ test("key for getItem converted to a string", function() {
         var value = pesky_keys[x][0];
         var repr = pesky_keys[x][1];
 
-        store.setItem(repr, repr);
-        same(store.getItem(value), repr, repr + " used as a key");
+        teststore.setItem(repr, repr);
+        same(teststore.getItem(value), repr, repr + " used as a key");
     }
 });
 
@@ -42,27 +40,27 @@ test("key/value for setItem converted to a string (Firefox 3.5 incompatibility)"
         for (var x=0; x < pesky_keys.length; x+=1) {
             var value = pesky_keys[x][0];
             var repr = pesky_keys[x][1];
-            store.setItem(str, value);
-            same(store.getItem(str), repr,
+            teststore.setItem(str, value);
+            same(teststore.getItem(str), repr,
                  repr + " given as the value");
-            store.setItem(value, str);
-            same(store.getItem(repr), str, repr + " set as the key");
+            teststore.setItem(value, str);
+            same(teststore.getItem(repr), str, repr + " set as the key");
         }
     });
 
 test("removeItem removes items", function() {
     var key = "some key";
-    store.setItem(key, "42");
+    teststore.setItem(key, "42");
 
-    ok(store.getItem(key), "key set");
+    ok(teststore.getItem(key), "key set");
 
-    store.removeItem(key);
-    same(store.getItem(key), null, "key removed");
+    teststore.removeItem(key);
+    same(teststore.getItem(key), null, "key removed");
 });
 
 test("removeItem a no-op if key does not exist", function() {
-    store.clear();
-    store.removeItem("some key that doesn't exist");
+    teststore.clear();
+    teststore.removeItem("some key that doesn't exist");
 });
 
 test("removeItem converts the key to a string", function() {
@@ -70,9 +68,9 @@ test("removeItem converts the key to a string", function() {
         var value = pesky_keys[x][0];
         var repr = pesky_keys[x][1];
 
-        store.setItem(repr, repr);
-        store.removeItem(value);
-        same(store.getItem(repr), null, repr + " removed");
+        teststore.setItem(repr, repr);
+        teststore.removeItem(value);
+        same(teststore.getItem(repr), null, repr + " removed");
     }
 });
 
@@ -81,57 +79,57 @@ test("'clear' removes all keys", function() {
     var key2 = "key2";
     var value = "42";
 
-    store.clear();
+    teststore.clear();
 
-    store.setItem(key1, value);
-    store.setItem(key2, value);
+    teststore.setItem(key1, value);
+    teststore.setItem(key2, value);
 
-    same(store.getItem(key1), value, "first key set");
-    same(store.getItem(key2), value, "second key set");
+    same(teststore.getItem(key1), value, "first key set");
+    same(teststore.getItem(key2), value, "second key set");
 
-    store.clear();
-    same(store.getItem(key1), null, "first key gone");
-    same(store.getItem(key2), null, "second key gone");
+    teststore.clear();
+    same(teststore.getItem(key1), null, "first key gone");
+    same(teststore.getItem(key2), null, "second key gone");
 });
 
 test("'clear' is a no-op if no keys exist", function() {
-    store.clear();
-    store.clear();
+    teststore.clear();
+    teststore.clear();
 });
 
 test("length", function() {
-    store.clear();
+    teststore.clear();
 
-    same(store.length, 0, "length == 0 after a clear");
+    same(teststore.length, 0, "length == 0 after a clear");
 
     var key = "key";
-    store.setItem(key, "42");
-    same(store.length, 1, "length++ after a set")
+    teststore.setItem(key, "42");
+    same(teststore.length, 1, "length++ after a set")
 
-    store.setItem(key, "-13");
-    same(store.length, 1,
+    teststore.setItem(key, "-13");
+    same(teststore.length, 1,
             "length unchanged when changing the value of an existing key");
 
-    store.removeItem(key);
-    same(store.length, 0, "length-- after a removal");
+    teststore.removeItem(key);
+    same(teststore.length, 0, "length-- after a removal");
 
-    store.setItem(key, "42");
-    same(store.length, 1, "length++ after another set");
-    store.clear();
-    same(store.length, 0, "length == 0 after another clear");
+    teststore.setItem(key, "42");
+    same(teststore.length, 1, "length++ after another set");
+    teststore.clear();
+    same(teststore.length, 0, "length == 0 after another clear");
 });
 
 test("'key' works for all known keys", function() {
     var keys = {key1: "42", key2: "-13", key3: "6"};
     var key_count = 3;
 
-    store.clear();
+    teststore.clear();
     for (var key in keys) {
-        store.setItem(key, keys[key]);
+        teststore.setItem(key, keys[key]);
     }
 
-    for (var x=0; x < store.length; x+=1) {
-        key = store.key(x);
+    for (var x=0; x < teststore.length; x+=1) {
+        key = teststore.key(x);
         ok(keys[key], "key returned by 'key' is valid");
         key_count -= 1;
     }
@@ -142,48 +140,48 @@ test("'key' works for all known keys", function() {
 test("'key' stable as long as no keys added/removed", function() {
     var keys = {key1: -1, key2: -1, key3: -1};
 
-    store.clear();
+    teststore.clear();
     for (var key in keys) {
-        store.setItem(key, "42");
+        teststore.setItem(key, "42");
     }
 
-    for (var x=0; x < store.length; x+=1) {
-        keys[store.key(x)] = x;
+    for (var x=0; x < teststore.length; x+=1) {
+        keys[teststore.key(x)] = x;
     }
 
-    store.setItem("key2", "0");
+    teststore.setItem("key2", "0");
     for (key in keys) {
-        same(store.key(keys[key]), key, key + " has the same index");
+        same(teststore.key(keys[key]), key, key + " has the same index");
     }
 });
 
 test("'key' adjusts properly when a key is removed", function() {
-    store.clear();
-    store.setItem("key 1", "1");
-    store.setItem("key 2", "2");
-    store.setItem("key 3", "3");
+    teststore.clear();
+    teststore.setItem("key 1", "1");
+    teststore.setItem("key 2", "2");
+    teststore.setItem("key 3", "3");
 
-    same(store.length, 3, "three keys stored");
+    same(teststore.length, 3, "three keys stored");
 
-    var key = store.key(1);
-    store.removeItem(key);
-    same(store.length, 2, "two keys left");
+    var key = teststore.key(1);
+    teststore.removeItem(key);
+    same(teststore.length, 2, "two keys left");
 
-    ok(store.key(1) !== key, "key(1) changed");
+    ok(teststore.key(1) !== key, "key(1) changed");
 });
 
 test("'key' returns null when given an index >= the # of keys " +
      "(Firefox 3.5/Safari 4 incompatibility", function() {
-    store.clear();
+    teststore.clear();
 
     // Specification says nothing about negative values
     var indices = [0, 1];
 
     for (var x=0; x < indices.length; x+=1) {
-        ok(indices[x] >= store.length,
+        ok(indices[x] >= teststore.length,
             "index " + indices[x] + " >= the # of keys");
         try {
-            same(store.key(indices[x]), null,
+            same(teststore.key(indices[x]), null,
                     "null returned for index " + indices[x]);
         }
         catch (e) {

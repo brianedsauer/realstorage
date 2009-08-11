dd093be2c6fce50767299e2e8ec42a530598fb07
Brett Cannon <brett@python.org>
2009-08-10 20:51:24 -0700

    Test for realStorage.keysArray.

--- tests/non-standard.js
+++ tests/non-standard.js
@@ -20,4 +20,26 @@ test("'contains' returns true for a value of null", function() {
         "'null' as a value for an existing key is OK");
 });
 
+
+test("keysArray", function() {
+    var keys_array = realStorage.keysArray();
+
+    same(keys_array, [], "empty store returns an empty array");
+
+    var keys = {"a": "a", "b": "b", "c": "c"};
+
+    for (var x in keys) {
+        realStorage.setItem(x, x);
+    }
+
+    var keys_array = realStorage.keysArray();
+    var found_keys = {};
+    
+    for (x in keys_array) {
+        found_keys[x] = x;
+    }
+
+    same(found_keys, keys, "all keys returned in array");
+});
+
 })();

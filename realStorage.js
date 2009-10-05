9554284c8e83871683ee43277531a73ec77c73be
Brett Cannon <brett@python.org>
2009-10-04 21:48:51 -0700

    Revert the code back to universally dealing with compatibility problems as performance is better/equal and code is simpler.

--- realStorage.js
+++ realStorage.js
@@ -1,160 +1,154 @@
+"use strict";
 /*
     realStorage -- provide a consistent API for localStorage.
     http://realstorage.googlecode.com/
 */
-"use strict";
-
 (function() {
 
-// No need to re-initialize or both if no localStorage
-if (window.realStorage || window.localStorage === undefined) {
+// No need to re-initialize
+if (window.realStorage) {
     return;
 }
 
-// Test for browser incompatibilities
-compat = (function() {
-    var compat = {};
-    var x = 0;
-    var test_attr = null;
+// localStorage is a pre-requisite
+if (!window.localStorage) {
+    throw new Error("realStorage requires localStorage");
+}
 
-    do {
-        test_attr = [" realStorage", x].join('');
-    } while (localStorage.getItem(test_attr) !== null);
+function wrapStorageArea(storageArea) {
 
-    /* 
-        Is null coerced?
+    var wrapper = {
 
-        Firefox 3.5
-    */
-    localStorage.setItem(test_attr, null);
-    compat.coerce_null = localStorage.getItem(test_attr) !== null;
+        key: function key(index) {
+            /*
+                (index:ulong) -> String
+
+                Return the key for the entry at the specified index.
+
+                If index is >= the length of the store, then return null.
+            */
+            if (index >= this.length) {
+                return null;
+            }
+            else {
+                return storageArea.key(index);
+            }
+        },
+
+        getItem: function getItem(key) {
+            /*
+               (key:String) -> String
+
+               Return the value stored under the specified key.
+
+               The key is converted to a string before being used to query the
+               store.
+            */
+            return storageArea.getItem(String(key));
+        },
+
+        setItem: function setItem(key, value) {
+            /*
+               (key:String, value:String) -> null
+
+               Set the key to the given value.
+
+               Both key and value are converted to strings before being stored.
+            */
+            storageArea.setItem(String(key), String(value));
+        },
+
+        removeItem: function removeItem(key) {
+            /*
+               (key:String) -> null
+
+               Remove the key and its associated value.
+
+               The key is converted to a string before being used.
+            */
+            storageArea.removeItem(String(key));
+        },
+
+        clear: function clear() {
+            /*
+               () -> null
+
+               Reset the store.
+            */
+            storageArea.clear();
+        },
+
+        /* NON-STANDARD */
+
+        contains: function contains(key) {
+            /*
+               (key:String) -> Boolean
+
+               Return true/false based on whether the key exists in the store.
+            */
+            return wrapper.getItem(key) !== null;
+        },
+
+        keysArray: function keysArray() {
+            /*
+               () -> Array
+
+               Return an array of all keys.
+            */
+            var keys = [];
+
+            for (var x=0; x < wrapper.length; x+=1) {
+                keys.push(wrapper.key(x));
+            }
+
+            return keys;
+        },
+
+        getJSONObject: function getJSONObject(key) {
+            /*
+               (key:String, [*args]) -> Object
+
+               Return a parsed JSON object stored under the specified key.
+            */
+            var args = [wrapper.getItem(key)];
+
+            for (var x=1; x < arguments.length; x+=1) {
+                args.push(arguments[x]);
+            }
+
+            return JSON.parse.apply(null, args);
+        },
+
+        setJSONObject: function setJSONObject(key, value) {
+            /*
+               (key:String, value: Object, [*args]) -> null
+
+               Store a JSON-compatible object.
+            */
+            var args = [value];
+
+            for (var x=2; x < arguments.length; x+=1) {
+                args.push(arguments[x]);
+            }
+
+            storageArea.setItem(String(key), JSON.stringify.apply(null, args));
+        }
+    };
 
     /*
-        Does key(.length) return null?
-
-        Firefox 3.5, Safari 4.0
+       ulong
+       STANDARD
+       The number of keys in the store.
     */
-    try {
-        localStorage.key(localStorage.length);
-        compat.key_length = true;
-    }
-    catch (exc) {
-        compat.key_length = false;
-    }
+    // JSLint complains about ``get length()`` descriptor being invalid syntax.
+    // ECMA5: Make into a proper descriptor.
+    wrapper.__defineGetter__("length", function() {
+            return storageArea.length;});
 
-    // Cleanup
-    localStorage.removeItem(test_attr);
-
-    return compat;
-})();
-
-
-/* Compatibility requirements */
-if (compat.coerce_null) {
-    function getItem(key) {
-        return localStorage.getItem(key);
-    }
-
-    function setItem(key, value) {
-        localStorage.setItem(key, value);
-    }
-
-    function removeItem(key) {
-        localStorage.removeItem(key);
-    }
-}
-else {
-    function getItem(key) {
-        return localStorage.getItem(String(key));
-    }
-
-    function setItem(key, value) {
-        localStorage.setItem(String(key), String(value));
-    }
-
-    function removeItem(key) {
-        localStorage.removeItem(String(key));
-    }
-}
-
-if (compat.key_length) {
-    function key(index) {
-        return localStorage.key(index);
-    }
-}
-else {
-    function key(index) {
-        if (index >= localStorage.length) {
-            return null;
-        }
-        else {
-            return localStorage.key(index);
-        }
-    }
+    return wrapper;
 }
 
 
-/* Dependent on compatibility changes */
-function contains(key) {
-    return getItem(key) !== null;
-}
-
-function getJSONObject(key) {
-    var args = [getItem(key)];
-
-    for (var x=1; x < arguments.length; x+=1) {
-        args.push(arguments[x]);
-    }
-
-    return JSON.parse.apply(null, args);
-
-}
-
-function setJSONObject(key, value) {
-    var args = [value];
-
-    for (var x=2; x < arguments.length; x+=1) {
-        args.push(arguments[x]);
-    }
-
-    setItem(key, JSON.stringify.apply(null, args));
-}
-
-
-/* No compatibility dependencies */
-function clear() {
-    return localStorage.clear();
-}
-
-function keysArray() {
-    var keys = [];
-
-    for (var x=0; x < localStorage.length; x+=1) {
-        keys.push(localStorage.key(x));
-    }
-
-    return keys;
-}
-
-
-realStorage = {
-    // Standard
-    getItem: getItem,
-    setItem: setItem,
-    removeItem: removeItem,
-    key: key,
-    clear: clear,
-    // Extra
-    contains: contains,
-    keysArray: keysArray,
-    getJSONObject: getJSONObject,
-    setJSONObject: setJSONObject
-};
-// Standard
-realStorage.__defineGetter__("length", function() {
-    return localStorage.length;});
-
-
-window.realStorage = realStorage;
+window.realStorage = wrapStorageArea(window.localStorage);
 
 })();
